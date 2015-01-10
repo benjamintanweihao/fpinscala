2bad84f4942bfef73954d1a6d77a2f5d83c81d17
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-01-10 17:11:52 +0800

    2.2 done.

---
+++ ex_2.2.scala
@@ -0,0 +1,19 @@
+object MyModule {
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    if (as.length == 1) return true
+    else ordered(as(0), as(1)) && isSorted(as.tail, ordered) 
+  }
+
+  def main(args: Array[String]): Unit =  {
+    var trivial = Array(1)
+    println(isSorted(trivial, (x: Int, y: Int) => x < y))
+    val sorted = Array(1, 2, 3, 4)
+    println(isSorted(sorted, (x: Int, y: Int) => x < y))
+    val not_reverse_sorted = Array(1, 2, 3, 4)
+    println(isSorted(not_reverse_sorted, (x: Int, y: Int) => x > y))
+    val unsorted = Array(1, 2, 0, 4)
+    println(isSorted(unsorted, (x: Int, y: Int) => x < y))
+  }
+}
+
