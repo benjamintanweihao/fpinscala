3dde3bd921bdedb49b81cf6111ae0f6939142c4d
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-01-10 18:21:48 +0800

    Ex 2.5

--- ex_2.5.scala
+++ ex_2.5.scala
@@ -1,6 +1,14 @@
-object MyModule {
+object Ex_2_5 {
 
   def compose[A,B,C](f: B => C, g: A => B): A => C = {
     a => f(g(a))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val f = (x: Double) => math.Pi / 2 - x
+    val cos1 = f andThen math.sin
+    val cos2 = compose(math.sin, f)
+    println(cos1(10))
+    println(cos2(10))
   }
 }
